18bf8b1ae5e6dd793bdf43757dc38d890e046b9d
ProstStuff <109490132+ProstStuff@users.noreply.github.com>
2026-04-04 14:42:50 +0700

    Editor stage 1 + readme

---
+++ src/main/java/dev/proststuff/skinmatics/client/gui/SkinmaticsPreview.java
@@ -0,0 +1,16 @@
+package dev.proststuff.skinmatics.client.gui;
+
+import dev.proststuff.skinmatics.client.SkinmaticsTextureManager;
+import org.jetbrains.annotations.Nullable;
+
+public class SkinmaticsPreview {
+    @Nullable public SkinmaticsTextureManager.SkinmaticsTextureData textureData;
+    public int x = -1;
+    public int y = -1;
+    public int width = -1;
+    public int height = -1;
+
+    public SkinmaticsPreview() {
+
+    }
+}
